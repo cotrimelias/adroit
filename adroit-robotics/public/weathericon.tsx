ef6ce5b6344dd245ad176e2517d22335000686b5
Suporte Digital Seller <cotrim.elias@gmail.com>
2022-01-20 18:05:42 -0300

    first commit

---
+++ adroit-robotics/public/weathericon.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Lottie from 'react-lottie'
+import animationData from './weathericon.json'
+
+const WeatherIcon = () => {
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+    }
+
+    return <Lottie 
+    options={defaultOptions}
+    height={300}
+    width={300}
+    ></Lottie>
+}
+
+export default WeatherIcon
